fdf93684a851d050128217d529c248391868c38a
Wiredbrat <vermaatul291002@gmail.com>
2025-05-28 23:15:32 +0530

    Updated Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react'
 import { Link, NavLink } from 'react-router'
 import Searchbar from './Searchbar'
-import { MenuIcon } from 'lucide-react'
+import { MenuIcon, SearchIcon } from 'lucide-react'
 
 function Header() {
 
@@ -75,24 +75,33 @@ function Header() {
           className='inline-flex self-center p-3 cursor-pointer hover:bg-[#0000002b] hover:duration-900 rounded-full duration-200'
           onClick={showSearchElement}
           >
-            <img src='src/assets/search-icon.png' className='invert h-4 md:h-[22px] aspect-square' alt="search icon"/>
+            <SearchIcon className='invert h-5 sm:h-8 md:h-[22px] aspect-square' alt="search icon"/>
           </span>
           <span className="hidden md:inline-flex p-3 self-center invert hover:bg-[#dadada2b] hover:duration-900 rounded-full duration-200">
             <a href="https://github.com/wiredbrat" target='_blank'>
-              <img src="src/assets/github.svg" alt="Github icon" className="h-7" />
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" alt="Github icon" className="h-7" ><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
             </a>
           </span>
           <span className='md:hidden invert flex justify-center items-center'>
-            <MenuIcon 
-            onClick={() => setShowMenu((prev) => !prev)}/>
+            <MenuIcon className={`${showMenu ? 'text-red-400': 'text-black'}`} 
+            onClick={() => {setShowMenu((prev) => !prev)
+            }}/>
           </span>
         </div>
       </div>
       
-
-      <div ref={mobileMenu} className={`${showMenu ? 'translate-x-[0%]' : 'translate-x-[-100%]'} mobileMenu h-screen w-[50%] bg-gray-500/30 backdrop-blur-lg duration-500 z-[99999] fixed left-0 md:hidden grid grid-rows-2 shadow-md`}
+      <div 
+      ref={mobileMenu} 
+      className={`${showMenu ? 'translate-x-[0%]' : 'translate-x-[-100%]'} w-[100%] h-screen bg-transparent fixed left-0 md:hidden grid z-[99999] duration-500  `}
       onClick={() => setShowMenu((prev) => !prev)}
       >
+
+      <div 
+      ref={mobileMenu} 
+      className={`${showMenu ? 'translate-x-[0%]' : 'translate-x-[-100%]'} mobileMenu h-screen w-[50%] bg-gray-500/30 backdrop-blur-lg duration-500 z-[99999] fixed left-0 md:hidden grid grid-rows-2 shadow-lg shadow-black`}
+      onClick={() => setShowMenu((prev) => !prev)}
+      >
+      
 
         <ul className="flex flex-col items-start gap-2 sm:gap-8 text-sm md:text-lg font-light m-auto ">
           <NavLink to='/' className={({isActive}) => `hover:text-blue-500 hover:duration-200 ${isActive? 'text-blue-500': 'text-white'}`}>
@@ -121,14 +130,15 @@ function Header() {
             <li className="cursor-pointer">More</li>
           </NavLink>
         </ul> 
-        <span className="md:hidden inline-flex hover:bg-[#dadada2b] hover:duration-900 rounded-full duration-200 absolute bottom-2 left-6">
+        <span className="md:hidden inline-flex hover:bg-[#dadada2b] hover:duration-900 rounded-full duration-200 absolute bottom-2 left-3">
           <a href="https://github.com/wiredbrat" target='_blank'>
-            <img src="src/assets/github.svg" alt="Github icon" className="h-7" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" alt="Github icon" className="h-7 invert" ><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
           </a>  
         </span>
-        <span className='text-[12px] absolute bottom-0 right-2'>
+        {/* <span className='text-[12px] absolute bottom-0 right-2'>
           made by WiredBrat
-        </span>
+        </span> */}
+      </div>
       </div>
       {/* <div className="pt-20 bg-slate-800 text-center text-2xl">
         sss
