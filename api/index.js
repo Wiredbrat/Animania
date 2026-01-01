eb8fa4a167b1afe26e8247381cfecc82909dbf7a
Wiredbrat <vermaatul291002@gmail.com>
2026-01-01 23:00:15 +0530

    backend db connection and server setup

---
+++ api/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from 'dotenv';
+import { dbConnection } from "./db/dbConnection";
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors({
+  origin: [`http://localhost:5173`],
+  credentials: true
+}))
+app.use(cookieParser())
+// app.use(express.json())
+
+dbConnection()
+.then(() => {
+  app.listen(port, () => {
+  console.log('App is running on', port)
+})})
+.catch((error) => {
+  console.log('server not started due to db connection failure')
+})
