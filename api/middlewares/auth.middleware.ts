b10364a250efc80b451cd736387e448050ab17e6
Wiredbrat <vermaatul291002@gmail.com>
2026-01-04 23:04:23 +0530

    feat: implement authentication middleware for token verification and user refresh

---
+++ api/middlewares/auth.middleware.ts
@@ -0,0 +1,57 @@
+import { asyncHandler } from "../utils/asyncHandler.ts";
+import { User } from '../models/user.model.ts'
+import { ApiError } from "../utils/ApiError.ts";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+asyncHandler(async(req, res, next) => {
+  const token = req.cookies.accessToken;
+
+  if(!token) {
+    throw new ApiError(401, "access token not found")
+  }
+  
+  let decodedToken;
+
+  try {
+    decodedToken = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET!);
+  } catch (error) {
+    const refreshToken = req.cookies?.refreshToken;
+
+    if(!refreshToken) {
+      throw new ApiError(401, "refresh token not found")
+    }
+
+    let decodedRefreshToken;
+
+    try {
+      decodedRefreshToken = jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN_SECRET!) as JwtPayload
+
+    } catch (error) {
+      throw new ApiError(401, "unauthorized user")
+    }
+    const user = await User.findById(decodedRefreshToken._id)
+
+    if(!user) {
+      throw new ApiError(404, "User not found");
+    }
+
+    const updatedRefreshToken = await user.generateRefreshToken();
+    const updatedAccessToken = await user.generateAccessToken();
+
+    user.accessToken = updatedAccessToken;
+    user.refreshToken = updatedRefreshToken;
+    user.save();
+
+    res.cookie("accessToken", updatedAccessToken, {
+      httpOnly: true,
+      secure: true
+    })
+
+    res.cookie("refreshToken", updatedRefreshToken, {
+      httpOnly: true,
+      secure: true
+    })
+
+    next();
+  }
+}) 
