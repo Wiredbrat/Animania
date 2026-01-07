7fedc2723f71809118080caff00e4926d911a74b
Wiredbrat <vermaatul291002@gmail.com>
2026-01-07 23:23:10 +0530

    feat: enhance user model with anime list and password verification; update asyncHandler and responseHandler for consistency

--- api/utils/asyncHandler.ts
+++ api/utils/asyncHandler.ts
@@ -1,9 +1,9 @@
 import type {Request, Response, NextFunction, RequestHandler} from "express";
 
 export const asyncHandler = 
-  (responseHandler: RequestHandler): RequestHandler => async(req: Request, res: Response, next: NextFunction) => {
+  (ApiResponse: RequestHandler): RequestHandler => async(req: Request, res: Response, next: NextFunction) => {
     try {
-      await responseHandler(req, res, next);
+      await ApiResponse(req, res, next);
     } catch (error) {
       console.table(`Error : ${error}`)
       next(error);
--- api/utils/responseHandler.ts
+++ api/utils/responseHandler.ts
@@ -4,14 +4,16 @@
 //   data: unknown
 // }
 
-export class responseHandler{
+export class ApiResponse{
   statusCode: number;
   message: string;
+  success: boolean;
   data: unknown
 
   constructor(statusCode: number, message: string, data?: unknown) {
     this.statusCode = statusCode;
     this.message = message;
+    this.success = true;
     this.data = data;
   }
 }
